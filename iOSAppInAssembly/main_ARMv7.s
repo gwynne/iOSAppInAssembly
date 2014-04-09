aba962f6522b3b388d5c393eecb3de7a884f9c50
Gwynne Raskind <gwynne@darkrainfall.org>
2014-04-09 15:55:01 -0500

    Fix.

--- iOSAppInAssembly/main_ARMv7.s
+++ iOSAppInAssembly/main_ARMv7.s
@@ -5,10 +5,23 @@
 // Then, we will setup an auto-release pool to manage our dangling objects,
 // And finally, we will jump right into UIApplicationMain.
 
-// Create a raw C string to hold the data for the App Delegate
-.section __TEXT,__cstring
-s_delegateClassNameCStr:
+// Create a raw C string to hold the data for the App Delegate class name
+	.section __TEXT,__cstring,cstring_literals
+l_.delegateClassNameCStr:
     .asciz "AppDelegate"
+
+// Create an CFString/NSString wrapper around the C string
+	.section __DATA,__cfstring
+L_delegateClassNameString:
+	.align 2
+	// isa
+	.long ___CFConstantStringClassReference
+	// cfinfo
+	.long 1992
+	// buffer
+	.long l_.delegateClassNameCStr
+	// length
+	.long 11
 
 //
 // Entry point of the application.
@@ -22,73 +35,49 @@ s_delegateClassNameCStr:
 // 
 .section __TEXT,__text,regular,pure_instructions
 .global _main
-.align 4
+.align 2
 _main:
-    // Note: This was taken from the official ARMv7 calling conventions document:
-    // https://developer.apple.com/library/ios/documentation/Xcode/Conceptual/iPhoneOSABIReference/Articles/ARMv7FunctionCallingConventions.html#//apple_ref/doc/uid/TP40009022-SW5
-    // I will assume that it is the correct way to start and finish functions throughout the project.
+	// Set up a frame pointer and save any non-volatile (callee-saved) registers used by the function.
     push     {r4-r7, lr}           // save LR, R7, R4-R6
     add      r7, sp, #12           // adjust R7 to point to saved R7
-    push     {r8, r10, r11}        // save remaining GPRs (R8, R10, R11)
-    vstmdb   sp!, {d8-d15}         // save VFP/Advanced SIMD registers D8
 
-    // Setup the autorelease pool. This is done using the (non-documented) function objc_autoreleasePoolPush.
-    //
-    // It takes no parameters.
-    //
-    // It returns no results.
-    bl _objc_autoreleasePoolPush
+	// Save off argc and argv (r0 and r1), we need them later and r0 and r1 can be clobbered in subroutines
+	mov		 r4, r0
+	mov		 r5, r1
 
-    // Setup our app delegate's class. This is done using our function AppDelegate_Setup.
-    //
-    // It takes no parameters.
-    //
-    // It returns no results.
-    bl AppDelegate_Setup
+    // Push an autorelease pool. This is done using the internal function objc_autoreleasePoolPush.
+    // It takes no parameters and returns no results.
+    blx _objc_autoreleasePoolPush
 
-    // Setup our custom view's class. This is done using our function View_Setup.
-    //
-    // It takes no parameters.
-    //
-    // It returns no results.
-    bl View_Setup
+	// Setup our app delegate's class.
+    // It takes no parameters and returns no results.
+    blx AppDelegate_Setup
 
-    // Next, we need to actually start up the visual application. This is done in 2 steps.
-    //
-    // 1: Create a Objective-C string from a C-String for our App Delegate's name.
-    // 2: Start the visual application.
+    // Setup our custom view's class.
+    // It takes no parameters and returns no results.
+    blx View_Setup
 
-    // Turn our C string into a NSString, using our function util_getCFString.
-    //
-    // Parameter 1 (r0) is the string we wish to convert.
-    //
-    // The newly created string object is stored in r0 after calling the function.
-    mov r0, s_delegateClassNameCStr
-    bl util_getCFString
+	// Call UIApplicationMain.
+	// It takes four parameters (main's argc and argv, a nib name, and a delegte class name), and returns
+	//	the value main should return.
+	// This sequence loads a PC-relative value into r3
+	movw r3, :lower16:(L_delegateClassNameString-(l_.stringLoad+8))
+	movt r3, :upper16:(L_delegateClassNameString-(l_.stringLoad+8))
+l_.stringLoad:
+	add r3, pc
 
-    // The next step is to start the visual application. This is done using the function UIApplicationMain.
-    //
-    // Parameter 1 (r0) is the argument count passed to main. For the purposes of this demo, it is irrelevant.
-    // Parameter 2 (r1) is the argument list passed to main. For the purposes of this demo, it is irrelevant.
-    // Parameter 3 (r2) is the name of the principle class. This would be a subclass of UIApplication if we had one.
-    // Parameter 4 (r3) is the name of the delegate class. This is our AppDelegate string.
-    //
-    // The result code of the application is stored in r0 after calling the function.
-    mov r3, r0
-    mov r2, #0
-    mov r1, #0
-    mov r0, #0
-    bl _UIApplicationMain
+	mov r0, r4
+	mov r1, r5
+	mov r2, #0
+	blx _UIApplicationMain
+	mov r4, r0 // save off the return value
 
-    // Finally, we must clean up our auto-release pool. This is done using the function objc_autoreleasePoolPop.
-    //
-    // It takes no parameters.
-    //
-    // It returns no usable results. However, it clobbers r0, so we save and re-load that variable around it.
-    push {r0}
-    bl _objc_autoreleasePoolPop
-    pop {r0}
+	// Pop the autorelease pool using the internal function objc_autoreleasePoolPop.
+	// It takes no parameters, and returns no useful value.
+	blx _objc_autoreleasePoolPop
 
-    vldmia   sp!, {d8-d15}         // restore VFP/Advanced SIMD registers
-    pop      {r8, r10, r11}        // restore R8-R11
+	// reload the UIApplicationMain return value
+	mov r0, r4
+
+	// Tear down frame pointer
     pop      {r4-r7, pc}           // restore R4-R6, saved R7, return to saved LR
